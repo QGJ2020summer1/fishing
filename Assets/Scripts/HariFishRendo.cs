16445705d22cf5cd5b9be072e57f087d98483dc5
松本龍 <matsumotoryu@matsumotoryuunoMacBook-Air.local>
2020-09-08 17:27:20 +0900

    針にfishが触れた時に移動するスクリプトを停止させる処理をHariFishRendoに追加

---
+++ Assets/Scripts/HariFishRendo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HariFishRendo : MonoBehaviour
+{
+    public GameObject parent;
+    public GameObject child;
+    
+    public void OnTriggerEnter2d(Collider collision)
+    {
+        string layerName = LayerMask.LayerToName(collision.gameObject.layer);
+        if (layerName == "Cube")
+        {
+            child = collision.gameObject;
+            child.transform.parent = parent.transform;
+            //Debug.Log("接触");
+        }
+    }
+  void OnTriggerEnter(Collider other)
+    {
+        //接触しているオブジェクトのタグが"Player"のとき
+        if (other.CompareTag("Hari"))
+        {
+            True1.GetComponent<FishMovement>().enabled = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider collision)
+    {
+        child.transform.parent = null;
+    }
+}
