661c280d242051aec15e59c97f2e4c44783a6e06
superbirdyman <asd1535peanuts@gmail.com>
2020-09-08 10:32:50 +0900

    add score

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreCounter : MonoBehaviour {
+
+    public static ScoreCounter instance;
+    Text ui_scoreText;
+
+    int score;
+
+    void Start() {
+        ui_scoreText = GetComponent<Text>();
+        instance = this;
+        score = 0;
+        UpdateView();
+    }
+
+    public void AddScore() {
+        score++;
+        UpdateView();
+    }
+
+    void UpdateView() {
+        ui_scoreText.text = string.Format("{0:000}", score);
+    }
+
+
+}
